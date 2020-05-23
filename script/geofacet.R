ea36a184a7149d74982d38a99474ffba529ec6b8
MarEichler <martha.eichlersmith@gmail.com>
2020-05-23 18:49:23 -0500

    updated data

--- script/geofacet.R
+++ script/geofacet.R
@@ -35,12 +35,14 @@ facet_data <- gf_state_tidy %>%
   filter(date > x_min)
 
 plus3 <- facet_data %>%
-  filter(gf > 3) 
+  filter(gf > 2) 
 
 
 x_min <- min(facet_data$date)
 x_max <- max(facet_data$date)
 source("script/colors.R")
+
+caption <- "Points represent a growth rate greater than 2 on a given day."
 
 title <- paste("Daily Growth Rate over last", n_days, "days")
 subtitle <- paste(x_min, "to", x_max)
@@ -53,32 +55,31 @@ facet_map <- ggplot(facet_data, aes(date, gf)) +
   geom_point( data = plus3, aes(date, gf), color = "grey35", size = 1.5) + 
   scale_y_continuous(
       name = "Growth Factor"
-    , limits = c(0, 3)
+    , limits = c(0, 2)
     , expand = c(0, 0)
-    , breaks = c(0, 1, 2, 3)
-    , labels = c("0", "1", "2", "3")
+    , breaks = c(0, 1, 2)
+    , labels = c("0", "1", "2")
     , oob = scales::squish
   ) +
   scale_x_date(
-      name = NULL
-    , expand = c(0, 0)
+      expand = c(0, 0)
     , breaks = NULL
   ) +
+  xlab("Points represent a growth rate greater than 3 on a given day.") +
   coord_cartesian(clip = "off") +
   facet_geo(~state, grid = "us_state_grid1", labeller = adjust_labels) +
   labs(
     title = title
     , subtitle = subtitle
-    , caption = "Data Source: usafacts.org"
   ) +
   theme_minimal() + 
   theme(
       strip.text = element_text(margin = margin(3, 3, 3, 3), face = "bold")
-    , axis.line.x = element_blank()
     , panel.spacing.x = grid::unit(5, "pt")
     , panel.spacing.y = grid::unit(5, "pt")
     , panel.grid = element_blank()
     , axis.text = element_text(size = 7)
+    , axis.title.x = element_text(margin = margin(20, 0, 0, 0))
     , axis.ticks.length.y = unit(5, "pt")
     , axis.ticks = element_line(color = "grey75")
     , plot.title = element_text(face = "bold", hjust = 0.5)
