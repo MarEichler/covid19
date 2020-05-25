0ee4a6b0e8e4db588c733c6b70c51ea10fe19760
MarEichler <martha.eichlersmith@gmail.com>
2020-05-25 13:24:35 -0500

    add comments; add tidy datasets and save tidy versions to rda

--- script/calc_growth_factor.R
+++ script/calc_growth_factor.R
@@ -5,10 +5,8 @@ library(tidyverse)
 
 link <- "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv"
 
+#import link
 total_cases <- read.csv(link)
-
-
-
 
 #total number of columsn 
 last_col <- ncol(total_cases)
@@ -25,8 +23,11 @@ names(total_cases)[s_date:last_col] <- format(as.Date(names(total_cases)[s_date:
 #rename columns 
 names(total_cases)[info_cols] <- c("countyFIPS", "county_name", "state", "stateFIPS")
 
+#save unedited version of csv 
 write.csv(total_cases, "data/covid_confirmed_usafacts.csv")
+####################################################
 
+#GROWTH FACTOR FUNCTION 
 f_GrowthFactor <- function(vec){
   id <- vec[,1] #get id name
   tc <- vec[,-1] #remove id name; just todal cases
@@ -58,7 +59,9 @@ f_GrowthFactor <- function(vec){
   growth_factor <- cbind(id, gf)
   growth_factor
 }
+########### END OF GROWTH FACTOR FUNCTION ####
 
+#APPLY TO DATA FRAME 
 f_DataFrame <- function(df, vec_func){
   last_row <- nrow(df)
   datalist = list()
@@ -69,31 +72,39 @@ f_DataFrame <- function(df, vec_func){
   new_df <- bind_rows(datalist)
   new_df
 }
+###### END OF APPLY TO DATA FRAME FUNCTION #####
 
 
+### SUM UP COUNTIES FOR EACH STATE
 total_cases_state <- total_cases  %>% 
   select(-c(1:2, 4)) %>% 
   group_by(state) %>%
   summarise_at(vars(-group_cols()), sum)
-total_cases_state
 
 
+#REMOVE EXCESS INFO COLUMNS; REMOVE DATA NOT ASSIGNED TO A COUNTY 
 total_cases_county <- total_cases %>%
   select(-c(2:4)) %>%
   filter(countyFIPS != 0) %>%
   as_tibble()
 
-total_cases_county
 
-test <- total_cases_county[1,]
-
-f_GrowthFactor(test)
-
+#CALCULATE GROWTH FACTOR BY COUNTY 
 gf_county <- f_DataFrame(total_cases_county, f_GrowthFactor)
 save(gf_county, file = "data/gf_county.rda")
 
+#CALCULATE GROWTH FACTOR BY COUNTY 
 gf_state <- f_DataFrame(total_cases_state, f_GrowthFactor)
 save(gf_state,file="data/gf_state.rda")
 
+#MAKE DATA TIDY AND CHOP INTO GROUPS 
+gf_county_tidy <- gf_county %>%
+  pivot_longer(cols = c(-1),  names_to = "date", values_to = "gf") %>%
+  mutate(date = as.Date(date))
+save(gf_county_tidy, file = "data/gf_county_tidy.rda") 
 
+gf_state_tidy <- gf_state %>%
+  pivot_longer(cols = c(-1),  names_to = "date", values_to = "gf") %>%
+  mutate(date = as.Date(date)) 
+save(gf_state_tidy, file = "data/gf_state_tidy.rda")
 
