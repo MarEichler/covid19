0208ecea5564f58525a79ae5df37ad5163e03bcc
MarEichler <martha.eichlersmith@gmail.com>
2020-05-25 15:20:24 -0500

    add blue comp

--- script/colors.R
+++ script/colors.R
@@ -5,7 +5,11 @@ gf0_1   <- brewer.pal(5, "YlOrRd")[2]
 gf1_2   <- brewer.pal(5, "YlOrRd")[3]
 gf2plus <- brewer.pal(5, "YlOrRd")[4]
 
+blue_comp <- "#2085f0"
+
 
 state_abbrv_color <- "grey20"
 
 line_state <- "grey20"
+
+
