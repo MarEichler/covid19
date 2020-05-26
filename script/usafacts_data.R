2927be78ad673a6e44dab3ef852b86334a66e56c
MarEichler <martha.eichlersmith@gmail.com>
2020-05-26 14:10:50 -0500

    add gf, dp, and ma; import parameters; na.rm in ma calc

--- script/usafacts_data.R
+++ script/usafacts_data.R
@@ -1,4 +1,6 @@
-#library(tidyverse)
+library(tidyverse)
+
+source("script/parameters.R")
 
 #get total confirmed cases
 #download data here: https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/
@@ -48,4 +50,34 @@ covid19_US <- left_join(cases, deaths, by = c("date"))
 
 
 
+#growth rate 
+#number of rows; use in `for loop`  
+R <- nrow(covid19_US)
+
+#GROWTH FACTOR  
+covid19_US$growth_factor <- NA
+
+#calculate growth rate
+#start with 2 because when r=1; 1-1= 0 (don't have a 0th row)
+for (r in 2:R){
+  covid19_US$growth_factor[r] <- 
+    #Growth Factor = New Cases Today / New Cases Yesterday 
+    covid19_US$new_cases[r] /  covid19_US$new_cases[r-1] 
+}
+
+covid19_US <- covid19_US %>%
+  mutate(
+    growth_factor = ifelse(growth_factor == Inf, new_cases, growth_factor)
+  )
+######################################
+
+#DEATH PERCENTAGE  
+covid19_US <- mutate(covid19_US, death_percentage = total_deaths / total_cases)
+
+#calculate moving average 
+covid19_US$MA_growth_factor <- c(rep(NA, ma_k - 1), zoo::rollmean(covid19_US$growth_factor, k=ma_k, na.rm=TRUE))
+covid19_US$MA_new_cases <- c(rep(NA, ma_k - 1), zoo::rollmean(covid19_US$new_cases, k=ma_k, na.rm = TRUE))
+
+
 save(covid19_US, file = "data/covid19_US.rda")
+write.csv(covid19_US, "data/covid19_US.csv")
