45cfa4580a8cebe09305b403abc520cbb0f8af14
MarEichler <martha.eichlersmith@gmail.com>
2020-05-26 14:50:23 -0500

    new script with global parameters

---
+++ script/parameters.R
@@ -0,0 +1,11 @@
+#moving average - pick two weeks
+ma_k <- 14
+
+#beginning date for overview plots 
+startdate <- "2020-03-15"
+#3/18 - start 'lockdown' ; after large growth factors↕
+
+
+#plot parametrs
+title_size <- 14
+subtitle_size <- 12
