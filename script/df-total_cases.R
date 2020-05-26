d48f6bcd05b03c03286da2154fc9c281fa052929
MarEichler <martha.eichlersmith@gmail.com>
2020-05-26 14:50:08 -0500

    create total_cases df with all states/counties breakdown

---
+++ script/df-total_cases.R
@@ -0,0 +1,19 @@
+#get total confirmed cases
+#download data here: https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/
+link <- "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv"
+
+#import link
+total_cases <- read.csv(link)
+
+#total number of columsn 
+last_col <- ncol(total_cases)
+
+
+#rename date columns into date format 
+names(total_cases)[5:last_col] <- format(as.Date(names(total_cases)[5:last_col], format = "X%m.%d.%y"), format = "%Y-%m-%d")
+
+#rename columns 
+names(total_cases)[1:4] <- c("countyFIPS", "county_name", "state", "stateFIPS")
+
+#save unedited version of csv 
+write.csv(total_cases, "data/covid_confirmed_usafacts.csv")
