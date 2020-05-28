50c5ce4481a33fdf4e5073e10e01bf8f21a8ec1d
MarEichler <martha.eichlersmith@gmail.com>
2020-05-28 17:51:29 -0500

    move data wrangling into script

--- script/variable/parameters.R
+++ script/variable/parameters.R
@@ -7,7 +7,32 @@ ma_k <- 14
 startdate <- "2020-03-15"
 #3/18 - start 'lockdown' ; after large growth factors↕
 
+load("data/covid19_US.rda")
+covid19_US_adj <- covid19_US  %>%
+  #only look at data after a given start date
+  filter(date >= as.Date(startdate))
+
 
 #plot parametrs
 title_size <- 14
 subtitle_size <- 12
+
+current_date <- max(covid19_US_adj$date)
+
+x_min <- as.Date(startdate)-.5
+x_max <- as.Date(current_date)+1
+
+#ggplot set ups 
+date_scaling <- scale_x_date(
+  name = NULL
+  , labels = date_format("%b-%d")
+  , limits = c(x_min, x_max)
+  , expand = c(0, 0)
+)
+
+thousands_scaling <- scale_y_continuous(
+  name = NULL
+  , limits = c(0, NA)
+  , labels = scales::unit_format(unit = "k", scale = 1e-3, accuracy = 1, big.mark = ",")
+  , expand = c(0, 0)
+)
