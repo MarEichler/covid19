52b2b87ccc13e2a025a34eeb4f4974a4bfede7f9
MarEichler <martha.eichlersmith@gmail.com>
2020-06-02 14:46:19 -0500

    update data

--- script/dataframe/covid19_county.R
+++ script/dataframe/covid19_county.R
@@ -2,6 +2,9 @@ library(tidyverse)
 load("data/total_cases.rda")
 source("script/function/calc_growth_factor.R")
 source("script/function/calc_new_cases.R")
+source("script/function/calc_apply_to_df.R")
+source("script/function/calc_ma_gf.R")
+source("script/variable/gf_cut_info.R")
 
 ### counties 
 county_total_cases <- total_cases %>%
@@ -11,17 +14,20 @@ county_total_cases <- total_cases %>%
   summarise_at(vars(-group_cols()), sum)
 
 #calculate gf and make tidy 
-gf_counties <- f_DataFrame(county_total_cases, f_GrowthFactor) %>%
+gf_counties_wide <- f_DataFrame(county_total_cases, f_GrowthFactor)
+
+gf_counties <- gf_counties_wide %>%
   pivot_longer(cols = c(-1),  names_to = "date", values_to = "gf") %>%
   mutate(date = as.Date(date)) 
 
 #calculate new casaes and make tidy 
-nc_counties <- f_DataFrame(county_total_cases, f_NewCases) %>%
+nc_counties_wide <- f_DataFrame(county_total_cases, f_NewCases) 
+
+nc_counties <- nc_counties_wide %>%
   pivot_longer(cols = c(-1),  names_to = "date", values_to = "nc") %>%
   mutate(date = as.Date(date)) 
 
-#create one data set 
+#create one data set of tidy data
 covid19_county <- inner_join(gf_counties, nc_counties, by = c("countyFIPS" = "countyFIPS", "date" = "date"))
-
-
 save(covid19_county, file = "data/covid19_county.rda") # send to data folder 
+
--- script/function/calc_new_cases.R
+++ script/function/calc_new_cases.R
@@ -24,16 +24,3 @@ f_NewCases <- function(vec){
   new_cases 
 }
 ########### END OF GROWTH FACTOR FUNCTION ####
-
-#APPLY TO DATA FRAME 
-f_DataFrame <- function(df, vec_func){
-  last_row <- nrow(df)
-  datalist = list()
-  for (i in 1:last_row){
-    datalist[[i]] <- vec_func(df[i,])
-   #    print(i)
-  }
-  new_df <- bind_rows(datalist)
-  new_df
-}
-###### END OF APPLY TO DATA FRAME FUNCTION #####
