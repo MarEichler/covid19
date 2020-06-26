23b590cdb1b7af96a9234121ffdb6c130eb88466
MarEichler <martha.eichlersmith@gmail.com>
2020-06-25 20:56:14 -0500

    add trend plot

--- script/plot/state_facet_nc.R
+++ script/plot/state_facet_nc.R
@@ -31,51 +31,111 @@ adjust_labels <- as_labeller(
   }
 )
 
-ma_k <- 7
-
-min_date <- as.Date("2020-03-15")  #min(facet_data$date)
-
-facet_data <- covid19_state %>%
-  filter(date >= min(min_date - ma_k, as.Date("2020-01-22")))
-
-max_date <- max(facet_data$date)
-
-
-title <- paste("New Cases and", ma_k, "Day Moving Average of New Cases")
-subtitle <- paste(min_date, "to", max_date)
-
-facet_map <- ggplot(facet_data, aes(date, nc)) +
-  geom_col(alpha = 0.3) +
-  geom_ma(ma_fun = SMA, n = 7, color = blue_comp, linetype = "solid", size = 0.8) + #SMA = simple moving average
-  scale_y_continuous(
-      name = NULL
-    , expand = c(0, 0)
-  ) +
-  scale_x_date(
-      name = NULL
-    , expand = c(0, 0)
-    , breaks = NULL
-  ) +
-  facet_geo(~state, grid = "us_state_grid2" , labeller = adjust_labels, scales = "free") +
-  labs(
-    title = title
-    , subtitle = subtitle
-    , caption = "Scales are NOT FIXED\nThis plot is not to compare numbers state to state, but to look at curves and where cases are increasing or decreasing"
-  ) +
-  theme_minimal() +
-  coord_cartesian(xlim = c(min_date, NA)) +
-  theme(
+### FUNCTION FOR PLOT, BASED IN INPUT FOR DATE
+func_nc_plot <- function(df, min_date){
+  min_date <- as.Date(min_date)  
+  facet_data <- df %>%
+    filter(date >= min_date)
+  
+  max_date <- max(facet_data$date)
+  title <- paste("New Cases and 7-Day Moving Average of New Cases")
+  subtitle <- paste(min_date, "to", max_date)
+  
+  ggplot(facet_data, aes(date, nc)) + 
+    geom_col(alpha = 0.3) +
+    geom_line(aes(y = nc_ma7), color = blue_comp) + 
+    facet_geo(~state, grid = "us_state_grid2" , labeller = adjust_labels, scales = "free") +
+    scale_x_date(      name = NULL, breaks = NULL, expand = c(0, 0)) + 
+    scale_y_continuous(name = NULL, breaks = NULL, expand = c(0, 0)) +
+    theme_minimal() +
+    labs(
+      title = title
+      , subtitle = subtitle
+      , caption = "Scales are NOT FIXED\nThis plot is not to compare numbers state to state, but to look at curves and where cases are increasing or decreasing"
+    ) +
+    theme_minimal() +
+    theme(
       strip.text = element_text(margin = margin(3, 3, 3, 3), face = "bold")
-    , panel.spacing.x = grid::unit(5, "pt")
-    , panel.spacing.y = grid::unit(5, "pt")
-    , panel.grid = element_blank()
-    , axis.text = element_blank() #element_text(size = 7)
-    , axis.title.x = element_text(margin = margin(20, 0, 0, 0))
-    #, axis.ticks.length.y = unit(5, "pt")
-    , axis.ticks = element_blank() #element_line(color = "grey75")
-    , plot.title = element_text(face = "bold", hjust = 0.5)
-    , plot.subtitle = element_text(hjust = 0.5, size = 12)
-  ) 
+      , panel.spacing.x = grid::unit(5, "pt")
+      , panel.spacing.y = grid::unit(5, "pt")
+      , panel.grid = element_blank()
+      , axis.text = element_blank() #element_text(size = 7)
+      , axis.title.x = element_text(margin = margin(20, 0, 0, 0))
+      #, axis.ticks.length.y = unit(5, "pt")
+      , axis.ticks = element_blank() #element_line(color = "grey75")
+      , plot.title = element_text(face = "bold", hjust = 0.5)
+      , plot.subtitle = element_text(hjust = 0.5, size = 12)
+    ) 
+}
+#########################
 
 
-ggsave("img/state_facet_nc.png", plot = facet_map, width = 7, height = 5, units = c("in"), dpi = 300)
+state_facet_0315   <- func_nc_plot(covid19_state, "2020-03-15")
+
+ggsave("img/state_facet_0315.png", plot = state_facet_0315, width = 7, height = 5, units = c("in"), dpi = 300)
+
+
+
+df <- covid19_state
+min_date <- max(df$date) - 28
+
+
+
+func_nc_plot_color <- function(df, min_date){
+  min_date <- as.Date(min_date)  
+  max_date <- max(df$date)
+  
+  n_days <- max_date - min_date
+  
+  df_trend <-df %>%
+    filter(date == max_date | date == min_date) %>%
+    select(state, date, nc_ma7) %>%
+    pivot_wider(., names_from = date, values_from = nc_ma7) %>%
+    select(state, min_date = 2, max_date = 3) %>%
+    mutate(trend = ifelse(min_date <= max_date, "Increasing", "Decreasing"))
+  
+  facet_data <- df %>%
+    filter(date >= min_date) %>%
+    left_join(., df_trend, by = "state")
+  
+  title <- paste("Last", n_days, "days: New Cases and 7-Day Moving Average of New Cases")
+  subtitle <- paste("Difference between 7-day moving average of new cases of", min_date, "and", max_date)
+    #paste("Previous", n_days, "days")
+  
+  ggplot(facet_data, aes(date, nc)) + 
+    geom_col(alpha = 0.3) +
+    geom_line(aes(y = nc_ma7, color = trend)) + 
+    facet_geo(~state, grid = "us_state_grid2" , labeller = adjust_labels, scales = "free") +
+    scale_x_date(      name = NULL, breaks = NULL, expand = c(0, 0)) + 
+    scale_y_continuous(name = NULL, breaks = NULL, expand = c(0, 0)) +
+    scale_color_manual(
+        name = NULL
+      , values = c("forestgreen", "red")
+    )+
+    theme_minimal() +
+    labs(
+      title = title
+      , subtitle = subtitle
+      , caption = "Scales are NOT FIXED\nThis plot is not to compare numbers state to state, but to look at curves and where cases are increasing or decreasing"
+    ) +
+    theme_minimal() +
+    theme(
+      strip.text = element_text(margin = margin(3, 3, 3, 3), face = "bold")
+      , legend.position = "top"
+      , panel.spacing.x = grid::unit(5, "pt")
+      , panel.spacing.y = grid::unit(5, "pt")
+      , panel.grid = element_blank()
+      , axis.text = element_blank() #element_text(size = 7)
+      #, axis.title.x = element_text(margin = margin(20, 0, 0, 0))
+      , axis.ticks = element_blank() #element_line(color = "grey75")
+      , plot.title = element_text(face = "bold", hjust = 0.5)
+      , plot.subtitle = element_text(hjust = 0.5, size = 12)
+    ) 
+}
+
+state_facet_trend7 <-  func_nc_plot_color(covid19_state, max(covid19_state$date) -  7) #1 week
+state_facet_trend14 <- func_nc_plot_color(covid19_state, max(covid19_state$date) - 14) #2 weeks
+
+
+ggsave("img/state_facet_trend7.png",  plot = state_facet_trend7,  width = 7, height = 5, units = c("in"), dpi = 300)
+ggsave("img/state_facet_trend14.png", plot = state_facet_trend14, width = 7, height = 5, units = c("in"), dpi = 300)
