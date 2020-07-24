82678963c7cf383c08b81d87b6e7e64e4bb586fe
MarEichler <martha.eichlersmith@gmail.com>
2020-07-24 13:49:38 -0500

    2020-07-22 column has counts as factors rather than integers; change to read as int

--- script/dataframe/total_cases.R
+++ script/dataframe/total_cases.R
@@ -16,6 +16,9 @@ names(total_cases)[5:last_col] <- format(as.Date(names(total_cases)[5:last_col],
 #rename columns 
 names(total_cases)[1:4] <- c("countyFIPS", "county_name", "state", "stateFIPS")
 
+total_cases$`2020-07-22` <- as.integer(total_cases$`2020-07-22`)
+#this column has numbers as 'factors' rather than integers; need to fix 
+
 #save unedited version of csv 
 write.csv(total_cases, "data/covid_confirmed_usafacts.csv") #save a verson of original data in csv form 
 save(total_cases, file = "data/total_cases.rda")
