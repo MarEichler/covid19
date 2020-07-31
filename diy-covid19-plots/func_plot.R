f1dd56d60c2c67fc46b9fb570493fd8c242ffa29
MarEichler <martha.eichlersmith@gmail.com>
2020-07-31 15:52:20 -0700

    add new deaths and new cases as plots

--- diy-covid19-plots/func_plot.R
+++ diy-covid19-plots/func_plot.R
@@ -1,14 +1,8 @@
-library(RColorBrewer)
-
 ##### SET COLORS 
-gf0     <- "grey80"
-gf0_1   <- brewer.pal(5, "YlOrRd")[2]
-gf1_2   <- brewer.pal(5, "YlOrRd")[3]
-gf2plus <- brewer.pal(5, "YlOrRd")[4]
 blue_comp <- "#2085f0"
 
 ###### FUNCTION TO CREATE PLOTS (WITH MOVING AVERAGE) 
-gf_plot_ma <- function(plot_data, min_date, max_date){
+plot_ma <- function(plot_data, min_date, max_date){
   
   plot_nc <- ggplot(plot_data, aes(x=date, y=nc))+
     geom_col(alpha=0.3) +
@@ -23,17 +17,33 @@ gf_plot_ma <- function(plot_data, min_date, max_date){
         name = NULL
       , labels = scales::date_format("%b-%d")
     ) + 
-    scale_y_continuous(labels = comma) +
+    scale_y_continuous(labels = comma, limits = c(0, NA)) +
     theme_minimal()+
     theme( plot.title = element_text(hjust = 0.5), axis.title = element_blank())
   
-  plot_nc
+  plot_nd <- ggplot(plot_data, aes(x=date, y=nd))+
+    geom_col(alpha=0.3) +
+    geom_line(
+      aes(y = ma_nd)
+      , size = 1.25
+      , color = blue_comp
+      , alpha = 0.6
+    ) +
+    ggtitle("New Deaths and Moving Average") +
+    scale_x_date(
+      name = NULL
+      , labels = scales::date_format("%b-%d")
+    ) + 
+    scale_y_continuous(labels = comma, limits = c(0, NA)) +
+    theme_minimal()+
+    theme( plot.title = element_text(hjust = 0.5), axis.title = element_blank())
   
-} # END OF GF_PLOT_MA
+  plot_grid(plot_nc, plot_nd, nrow = 2, align = "v")
+  
+} # END OF MA + PLOTS 
 
 ###### FUNCTION TO CREATE PLOTS (NO MOVING AVERAGE) 
-gf_plot <- function(plot_data, min_date, max_date){
-
+plot <- function(plot_data, min_date, max_date){
   
   plot_nc <- ggplot(plot_data, aes(x=date, y=nc))+
     geom_col(alpha=0.3) +
@@ -42,10 +52,22 @@ gf_plot <- function(plot_data, min_date, max_date){
         name = NULL
       , labels = scales::date_format("%b-%d")
     ) + 
-    scale_y_continuous(labels = comma) +
+    scale_y_continuous(labels = comma, limits = c(0, NA)) +
     theme_minimal()+
     theme( plot.title = element_text(hjust = 0.5), axis.title = element_blank())
   
-  plot_nc 
+  plot_nd <- ggplot(plot_data, aes(x=date, y=nd))+
+    geom_col(alpha=0.3) +
+    ggtitle("New Deaths") +
+    scale_x_date(
+      name = NULL
+      , labels = scales::date_format("%b-%d")
+    ) + 
+    scale_y_continuous(labels = comma, limits = c(0, NA)) +
+    theme_minimal()+
+    theme( plot.title = element_text(hjust = 0.5), axis.title = element_blank())
   
-} # END OF GF_PLOT 
+  plot_grid(plot_nc, plot_nd, nrow = 2, align = "v") 
+
+  
+} # END OF JUST PLOTS 
