cb2301661aca7e01012601938cf2cecdaf9d89d8
MarEichler <martha.eichlersmith@gmail.com>
2020-07-31 15:52:33 -0700

    checking updated version of apps

--- diy-covid19-plots/testing/checking app.R
+++ diy-covid19-plots/testing/checking app.R
@@ -3,18 +3,28 @@ library(knitr)
 library(zoo)
 library(cowplot)
 library(scales)
+
+#scripts
+source("script/variable/colors.R")
+
+#DATA
 load("data/covid19_US.rda")
 load("data/covid19_state.rda")
-load("data/covid19_county.rda")
-source("script/variable/colors.R")
+
+
 
 us_all <- covid19_US %>%
   mutate(state = "Entire US") %>%
-  select(state, date, gf = growth_factor, nc = new_cases)
+  select(state, date, gf = growth_factor, nc = new_cases, nd = new_deaths)
+
 
 data <- covid19_state %>%
-  select(state, date, gf, nc) %>%
+  select(state, date, gf, nc, nd) %>%
+  mutate(state = as.character(state))
   rbind(., us_all)
+
+unique(covid19_state$state)
+
 
 
 state_pc <- unique(data$state)
@@ -32,45 +42,16 @@ date_scaling <- scale_x_date(
 
 ma_k <- 7
 
-min_date <- x_max -28
+min_date <- x_min
 max_date <- x_max
 
 plot_data <- data %>%
   filter(state == "MN") %>%
   mutate(
-      ma_gf = c(rep(NA, ma_k - 1), rollmean(gf, k=ma_k, na.rm=TRUE))
-    , ma_nc = c(rep(NA, ma_k - 1), rollmean(nc, k=ma_k, na.rm=TRUE))
+      ma_nc = c(rep(NA, ma_k - 1), rollmean(nc, k=ma_k, na.rm=TRUE))
+    , ma_nd = c(rep(NA, ma_k - 1), rollmean(nd, k=ma_k, na.rm=TRUE))
     ) %>%
-  filter(date >= min_date & date <= max_date) %>%
-  mutate(
-    gf_2plus = ifelse(gf > 2, 2, -1), 
-    ma_2plus = ifelse(ma_gf > 2, 2, -1)
-  )
-
-plot_gf <- ggplot(plot_data, aes(date, gf)) + 
-  annotate("rect", xmin = min_date, xmax = max_date, ymin = 1, ymax =  2, fill = "#F03B20", alpha = 0.2) +
-  geom_area(alpha = 0.3) + #raw gf
-  geom_line(
-    aes(y = ma_gf)
-    , size = 1.25
-    , color = blue_comp
-    , alpha = 0.6
-  ) +
-  geom_point(aes(date, gf_2plus), color = "grey35", size = 3) + #gf > 2
-  geom_point(aes(date, ma_2plus), color = blue_comp, size = 4, alpha = 0.3) + #ma_gf > 2
-  ggtitle("Growth Factor") +
-  scale_y_continuous(
-      breaks = c(0, 1, 2)
-    , labels = c("0", "1", "2")
- #   , expand = c(0, .02)
-  ) +
-  scale_x_date(
-    name = NULL
-    , labels = scales::date_format("%b-%d")
-  ) + 
-  coord_cartesian(ylim = c(0, 2)) +
-  theme_minimal() +
-  theme( plot.title = element_text(hjust = 0.5) , axis.title = element_blank())
+  filter(date >= min_date & date <= max_date) 
 
 plot_nc <- ggplot(plot_data, aes(x=date, y=nc))+
   geom_col(alpha=0.3) +
@@ -89,11 +70,34 @@ plot_nc <- ggplot(plot_data, aes(x=date, y=nc))+
   theme_minimal()+
   theme( plot.title = element_text(hjust = 0.5), axis.title = element_blank())
 
+plot_nd <- ggplot(plot_data, aes(x=date, y=nd))+
+  geom_col(alpha=0.3) +
+  geom_line(
+    aes(y = ma_nd)
+    , size = 1.25
+    , color = blue_comp
+    , alpha = 0.6
+  ) +
+  ggtitle("New Deaths") +
+  date_scaling + 
+  scale_y_continuous(
+    , labels = comma
+  ) +
+  theme_minimal()+
+  theme( plot.title = element_text(hjust = 0.5), axis.title = element_blank())
 
-plot_grid(plot_nc, plot_gf, align = "h")
+
+plot_grid(plot_nc, plot_nd, nrow = 2, align = "v")
+
+
 ##############################
 
  
-cnty_data <- covid19_county %>%
-  filter(countyFIPS == "1")
-cnty_data
+data %>% 
+  filter(state == "MN") %>%
+  summarize(
+      `Total Cases`  = formatC(sum(nc), big.mark=",", format="d")
+    , `Total Deaths` = formatC(sum(nd), big.mark=",", format="d")
+    ) %>%
+  kable(align = c("r", "r"), label = "MN")
+
